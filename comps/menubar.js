051001ddaf86f36340154e9e9650350cd8c9f7ec
marianfrancisco <mfrancisco9@my.bcit.ca>
2022-04-12 13:18:40 -0700

    menu bar updates

---
+++ comps/menubar.js
@@ -0,0 +1,26 @@
+import React from "react";
+import {HiHome, HiCheckCircle, HiShoppingBag, HiHeart} from 'react-icons/hi';
+import {GiHanger} from 'react-icons/gi';
+
+function Menubar() {
+    return <div className='menubar'>
+        
+        <div className='icons'>
+            <p> <HiHome size="35px" color="white"/> </p>
+            <p> <HiCheckCircle size="35px" color="white"/> </p>
+            <p> <HiShoppingBag size="35px" color="white"/> </p>
+            <p> <HiHeart size="35px" color="white"/> </p>
+            <p> <GiHanger size="35px" color="white"/> </p>
+        </div>
+
+        <div className='labels'>
+             <p> Home </p>
+             <p> Verify </p>
+             <p> Shopping </p>
+             <p> Saved </p>
+             <p> Tips </p>
+        </div>
+    </div>
+}
+
+export default Menubar
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
 import '../styles/globals.css'
+import Menubar from '../comps/menubar'
+import '../styles/menubar.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+  <>
+  <Component {...pageProps} />
+  <Menubar />
+  </>
+  )
 }
 
 export default MyApp
