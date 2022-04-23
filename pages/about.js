d31b976d501d170117e75bbfb99aa3a981c95ff2
marianfrancisco <mfrancisco9@my.bcit.ca>
2022-04-22 23:18:16 -0700

    about page updates

--- pages/about.js
+++ pages/about.js
@@ -1,8 +1,14 @@
 import React from "react";
 
 function About() {
-    return <div>
-        This is the about page
+    return <div class="aboutpage">
+        <h1 class="header">About Us</h1>
+        <p class ="pagedescription">Revived is a fashion conscious app developed by our team with a 
+            mission to help raise awarness on the negative impacts of 
+            the fast fashion industry and make more sustainable choices for 
+            fashion.This app also provides resources on how people can reuse their 
+            clothing or re-sell their clothing in the community page. 
+        </p>
     </div>
 }
 
