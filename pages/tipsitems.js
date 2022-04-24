fff37bfe643fc655e44d83d8c6e86cf67582940d
= <bbrowne14@my.bcit.ca>
2022-04-24 11:09:36 -0700

    more changes

--- pages/tipsitems.js
+++ pages/tipsitems.js
@@ -3,23 +3,49 @@ import ClothingImage from '../comps/clothingimage';
 import HeaderImage from '../comps/headerimage';
 import GreenBar from '../comps/greenbar';
 import BodyText from '../comps/bodytext';
+import {useRouter} from 'next/router';
 
-export default function Tipsitems() {
+export default function Tipsitems( ){
+    const r = useRouter();
     return <div className={styles.main}>
      
     <div className="pagetitle">
     <h1 className="header">Clothing Tips</h1>
     <div className="subhead">Fashion Hacks</div>
     <div className="rows">
-    <div className="row"><ClothingImage name="About Revived" /></div>
-    <div className="row"><ClothingImage  /></div>
-    <div className="row"><ClothingImage  /></div>
-    <div className="row"><ClothingImage  /></div>
-    <div className="row"><ClothingImage  /></div>
-    <div className="row"><ClothingImage  /></div>
+        <div className="row"><div onClick={
+                ()=>r.push("/tipsdescription")
+            }><ClothingImage /></div>
+            Shoe Scuff Marks
+        </div>
+        <div className="row"><div onClick={
+                ()=>r.push("/tipsdescription")
+            }><ClothingImage /></div>
+            Clean Shoes
+        </div>
+        <div className="row"><div onClick={
+                ()=>r.push("/tipsdescription")
+            }><ClothingImage /></div>
+            Dye Jeans
+        </div>
+        <div className="row"><div onClick={
+                ()=>r.push("/tipsdescription")
+            }><ClothingImage /></div>
+            Tie Dye
+        </div>
+        <div className="row"><div onClick={
+                ()=>r.push("/tipsdescription")
+            }><ClothingImage /></div>
+            Colour Shoes
+        </div>
+        <div className="row"><div onClick={
+                ()=>r.push("/tipsdescription")
+            }><ClothingImage /></div>
+            Shoe Scuff Marks
+        </div>
     </div>
+
     </div>
-    
     </div>
     
 }
