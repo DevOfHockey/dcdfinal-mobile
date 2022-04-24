c5977ea3a2244222aae49829710708ed7afb21d3
marianfrancisco <mfrancisco9@my.bcit.ca>
2022-04-24 11:32:29 -0700

    Shopping components and data

--- pages/shopping.js
+++ pages/shopping.js
@@ -1,11 +1,21 @@
 import styles from "../styles/Home.module.css";
+import { shopImgs } from "../data/shopitems";
+import GridImgs from "../comps/gridimgs";
 
 export default function Shopping() {
     return <div className={styles.main}>
      
     <div class="pagetitle">
-    <h1 class="header">Shopping</h1>
-    <p class="subheading">Browse second-hand clothing from our Community</p>
+        <h1 class="header">Shopping</h1>
+        <p class="subheading">Browse second-hand clothing from our Community</p>
     </div>
+
+    <div class="shopImgs">
+        {
+            //shopImgs.map((o,i)=><img src={o} height={180} />)
+        }
+        <GridImgs arr={shopImgs} />
+    </div>
+
     </div>
 }
