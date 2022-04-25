bc4e71c51564542209a465df8ead7190a8f20018
marianfrancisco <mfrancisco9@my.bcit.ca>
2022-04-25 10:21:35 -0700

    changes

--- comps/gridtext.js
+++ comps/gridtext.js
@@ -4,7 +4,6 @@ const DispCont = styled.div`
 display: grid;
 grid-row-gap: 140px;
 grid-column-gap: 40px;
-grid-gap: 50px;
 grid-template-rows: 50px 50px 50px;
 grid-template-columns: 150px 150px;
 justify-content: center;
