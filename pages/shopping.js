8fae9a9beff0ce597d94f768631dbea64514b9ea
marianfrancisco <mfrancisco9@my.bcit.ca>
2022-04-25 09:00:27 -0700

    updates

--- pages/shopping.js
+++ pages/shopping.js
@@ -2,10 +2,14 @@ import styles from "../styles/Home.module.css";
 import { shopImgs, shopLabels } from "../data/shopitems";
 import GridImgs from "../comps/gridimgs";
 import GridTxt from "../comps/gridtext";
+import {useRouter} from 'next/router';
 
 
 
 export default function Shopping() {
+    const r = useRouter();
+    const {page} = r.query;
+
     return <div className={styles.main}>
      
     <div class="pagetitle">
@@ -13,11 +17,22 @@ export default function Shopping() {
         <p class="subheading">Browse second-hand clothing from our Community</p>
     </div>
 
+<<<<<<< HEAD
     <div>
         <GridTxt arr={shopLabels} />    
     </div>
 
     <div class="shopImgs">
+=======
+    <div class="shopImgs" onClick={
+        ()=>r.replace({
+            pathname:"/denimjacket",
+            query:{
+                page:1
+            }
+        })
+    }>
+>>>>>>> 21f8d5f (shopping update)
         <GridImgs arr={shopImgs} />
     </div>
 
