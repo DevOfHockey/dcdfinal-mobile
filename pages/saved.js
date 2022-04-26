6a10d5519081025cd3ec8156fd37ca8b361a2cbc
DevOfHockey <liam.conkin@gmail.com>
2022-04-26 11:47:32 -0700

    fix saved

---
+++ pages/saved.js
@@ -0,0 +1,18 @@
+import Menubar from "../comps/menubar";
+import { savedImgs, savedTxt } from "../data/saveditems";
+import LinearImgs from "../comps/linearimgs";
+import SavedTxt from "../comps/savedtext";
+
+export default function Saved() {
+    return <div class="pagetitle">
+    <h1 class="header">Saved</h1>
+    <p class="subheading">Get back to your list of favorite fashion hacks and tips</p>
+
+    <div class="savedImgs">
+        <LinearImgs arr={savedImgs} />
+        <SavedTxt arr={savedTxt} />
+    </div>
+
+    <Menubar />
+    </div>
+}
