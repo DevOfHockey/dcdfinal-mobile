c5445ce01e7841c17e75ad179a487680bb0ca66c
= <bbrowne14@my.bcit.ca>
2022-04-26 09:18:19 -0700

    changes

--- pages/saved.js
+++ pages/saved.js
@@ -1,17 +1,9 @@
 import { savedImgs, savedTxt } from "../data/saveditems";
 import LinearImgs from "../comps/linearimgs";
 import SavedTxt from "../comps/savedtext";
-import {HiArrowNarrowLeft} from 'react-icons/hi';
-import {useRouter} from 'next/router';
 
 export default function Saved() {
-    const r = useRouter();
     return <div class="pagetitle">
-
-    <div className="arrow" onClick={
-            ()=>r.push("/shopping")
-        }><HiArrowNarrowLeft size="45px" color="black" /></div>
-
     <h1 class="header">Saved</h1>
     <p class="subheading">Get back to your list of favorite fashion hacks and tips</p>
 
